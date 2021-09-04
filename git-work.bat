89b1514a2dd48b6342fbb229bc71848ae07746e4
kbelyaev <clash@aport.ru>
2021-09-04 13:21:34 +0300

    Added file git-works.bat

---
+++ git-work.bat
@@ -0,0 +1,8 @@
+@echo off
+rem git config --global user.name "kbelyaev"
+rem git config --global user.name
+rem ssh-keygen -t ed25519 -C "clash@aport.ru"
+rem git config --global core.editor "C:/Tools/npp/notepad++.exe"
+rem git remote -v
+rem git remote set-url origin git@github.com:kbelyaev/TestK3.git
+rem git push --set-upstream origin master
